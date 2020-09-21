fac0a6ab298e700c13cfb9ea4c9e3a829c58f979
Aaron Ellis <Aaronde4@outlook.com>
2020-09-21 20:07:45 +0000

    work in progess pickachu

---
+++ studentWork/Unit1/PikachuChatter.java
@@ -0,0 +1,9 @@
+package studentWork.Unit1;
+public class PikachuChatter{
+    public static void main(String[] args){
+        System.out.println("(\__/)");
+        //System.out.println("(o^.^)");
+        //System.out.println("z(_(")(")")");
+        
+    }
+}
