1256df735c6f45218a51f35f14a59df41d351b80
Aaron Ellis <Aaronde4@outlook.com>
2020-09-24 20:20:12 +0000

    Programming Project 1 submission

---
+++ studentWork/Unit1/DrawMississippi.java
@@ -0,0 +1,52 @@
+package studentWork.Unit1;
+    //This
+public class DrawMississippi {
+    public static void main(String[] args){
+        bigM();
+        bigI();
+        bigS();
+        bigS();
+        bigI();
+        bigS();
+        bigS();
+        bigI();
+        bigP();
+        bigP();
+    }
+    public static void bigM () {
+        System.out.println("M       M");
+        System.out.println("MM     MM");
+        System.out.println("M M   M M");
+        System.out.println("M   M   M");
+        System.out.println("M       M");
+        System.out.println("M       M");
+        System.out.println("M       M");
+    }
+    public static void bigI () {
+        System.out.println("IIIII");
+        System.out.println("  I  ");
+        System.out.println("  I  ");
+        System.out.println("  I  ");
+        System.out.println("  I  ");
+        System.out.println("  I  ");
+        System.out.println("IIIII");
+    }
+    public static void bigS () {
+        System.out.println(" SSSS");
+        System.out.println("S    S");
+        System.out.println("S");
+        System.out.println(" SSSS");
+        System.out.println("     S");
+        System.out.println("S    S");
+        System.out.println(" SSSS");
+    }
+    public static void bigP () {
+        System.out.println("PPPPPP");
+        System.out.println("P     P");
+        System.out.println("P     P");
+        System.out.println("PPPPPP");
+        System.out.println("P");
+        System.out.println("P");
+        System.out.println("P");
+    }
+}
